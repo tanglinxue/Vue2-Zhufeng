f9aaf303c5147e8dd5ed7ddfde6c1cf07de992f3
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2023-06-16 17:39:01 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -12,4 +12,27 @@ Vue.prototype.$nextTick = nextTick
 initMixin(Vue)// 传递 Vue 的同时扩展了 _init 方法
 initLifeCycle(Vue)
 
+Vue.options = {}
+
+function mergeOptions(parent, child) {
+  const options = {}
+  for (let key in parent) {
+    mergeFiled(key)
+  }
+  for (let key in child) {
+    if (!parent.hasOwnPerperty(key)) {
+      mergeFiled(key)
+    }
+  }
+  function mergeFiled(key) {
+    options[key] = child[key] || parent[key];
+  }
+  return options
+}
+
+Vue.mixin = function (mixin) {
+  this.options = mergeOptions(this.options, mixin)
+  return this
+}
+
 export default Vue
